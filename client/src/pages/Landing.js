0773059d2054e5194577aee32aeee4668dc7af86
kyloReneo <saeedderayat2000@gmail.com>
2024-05-07 19:50:00 +0330

    logo changed

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,8 +1,30 @@
-import logo from "../assets/images/logo.svg";
+import logo from "../assets/images/Color logo with background.svg";
 import mainImage from "../assets/images/main.svg";
 
 const Landing = () => {
-  return <h4>landing page</h4>;
+  return (
+    <main>
+      <nav>
+        <img src={logo} alt="Joby" className="logo" />
+      </nav>
+      <div className="container page"></div>
+      {/* info */}
+      <div className="info">
+        <h1>
+          Job <span>Tracking</span> Web Application
+        </h1>
+        <p>
+          I'm baby blackbird spyplane godard tonx, photo booth grailed biodiesel
+          asymmetrical forage post-ironic four dollar toast hoodie literally tbh
+          cred. Keffiyeh art party sriracha, tofu vinyl kickstarter pinterest
+          cupping yr air plant shaman narwhal same. VHS cardigan squid, occupy
+          street art venmo austin four dollar toast umami adaptogen.
+        </p>
+        <butten className="btn btn-hero">Login/Register</butten>
+      </div>
+      <img src={mainImage} alt="Job hunt" className="img main-img" />
+    </main>
+  );
 };
 
 export default Landing;
