ad4249754cd9612e744198901fbeda91e4c1848f
kyloReneo <saeedderayat2000@gmail.com>
2024-05-10 14:23:54 +0330

    routes init

--- server.js
+++ server.js
@@ -1,23 +1,32 @@
-//Import express.js and create the express app
+// Import express.js and create the express app
 import express from "express";
 const app = express();
 
-//Read the configs from .env file
+// Read the configs from .env file
 import dotenv from "dotenv";
 dotenv.config();
 
+// Read database credentials from .env and make connection
 import connectDB from "./db/connect.js";
 const MongoUsername = encodeURIComponent(process.env.MONGO_USERNAME);
 const MongoPassword = encodeURIComponent(process.env.MONGO_PASSWORD);
-const MongoUri = `mongodb+srv://${MongoUsername}:${MongoPassword}@cluster0.mcbnsw1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const MongoUri = `mongodb+srv://${MongoUsername}:${MongoPassword}@cluster0.mcbnsw1.mongodb.net/Joby?retryWrites=true&w=majority&appName=Cluster0`;
 
-//Middlewares
+// Routers
+import authRouter from "./routes/authRoutes.js";
+
+// Middlewares
 import notFoundMiddleware from "./middleware/NotFound.js";
 import errorHandlerMiddleware from "./middleware/ErrorHandler.js";
+
+// Built-in express middleware for parsing json
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("init");
 });
+
+app.use("/api/v1/auth", authRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
