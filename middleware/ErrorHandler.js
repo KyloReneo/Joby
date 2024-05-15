b8d5574040539a51b08bdd867f0b27fd124a3569
kyloReneo <saeedderayat2000@gmail.com>
2024-05-15 11:27:08 +0330

    check unique email

--- middleware/ErrorHandler.js
+++ middleware/ErrorHandler.js
@@ -1,15 +1,22 @@
 import { StatusCodes } from "http-status-codes";
 
 const errorHandlerMiddleware = (err, req, res, next) => {
+  // Set a default error object
   const defaultError = {
     statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
     msg: "Something went wrong on the server side, please try again later...",
   };
+  // Check empty fields
   if (err.name === "ValidationError") {
     defaultError.statusCode = StatusCodes.BAD_REQUEST;
     defaultError.msg = Object.values(err.errors)
       .map((item) => item.message)
       .join(", ");
+  }
+  // Check if the entered email is unique
+  if (err.code && err.code == 11000) {
+    defaultError.statusCode = StatusCodes.BAD_REQUEST;
+    defaultError.msg = `${Object.keys(err.keyValue)} field has to be unique.`;
   }
   res.status(defaultError.statusCode).json({ msg: defaultError.msg });
   // res.status(defaultError.statusCode).json({ msg: err });
